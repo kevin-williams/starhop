3c3b026d00e45eeaa6ddd8fdcaf1d773922c1235
Kevin Williams <kwilliams@discounttire.com>
2017-12-01 11:09:21 -0700

    Added eyepiece view

--- src/containers/starhop/StarHop.jsx
+++ src/containers/starhop/StarHop.jsx
@@ -12,15 +12,15 @@ const ORION_QUERY = {
   raTo: 6.5,
   decFrom: -15,
   decTo: 15,
-  magLimit: 9,
+  magLimit: 10,
 }
 
 const PLEIADES_QUERY = {
-  raFrom: 3.6,
-  raTo: 4,
-  decFrom: 22,
-  decTo: 25,
-  magLimit: 9,
+  raFrom: 3.3,
+  raTo: 4.5,
+  decFrom: 20,
+  decTo: 27,
+  magLimit: 10,
 }
 
 // Take redux state and set it into the component properties for easy access
@@ -29,11 +29,16 @@ const mapStateToProps = state => state;
 export default class StarHop extends Component {
   render() {
     return (
-      <div className="starhop-location">
+      <div>
+        <div className="starhop-hopview">
         <ErrorLine statusList={[this.props.starhop.zipCodeStatus]} />
         <StarMap stars={this.props.starhop.stars} view={this.props.starhop.view}/>
-        <button onClick={() => this.props.getStars(ORION_QUERY)}>Load</button>
-        <button onClick={() => this.props.history.goBack()}>Back</button>
+        <StarMap stars={this.props.starhop.stars} view={this.props.starhop.eyepieceView}/>
+        </div>
+        <div>
+          <button onClick={() => this.props.getStars(PLEIADES_QUERY)}>Load</button>
+          <button onClick={() => this.props.history.goBack()}>Back</button>
+        </div>
       </div>
     );
   }
--- src/containers/starhop/starHopReducer.js
+++ src/containers/starhop/starHopReducer.js
@@ -11,21 +11,41 @@ const ORION_VIEW = {
     height: 600
 }
 
+const ORION_EYEPIECE_VIEW = {
+  raFrom: 4.5,
+  raTo: 6.5,
+  decFrom: -15,
+  decTo: 15,
+  magLimit: 11,
+  width: 300,
+  height: 300
+}
+
 const PLEIADES_VIEW = {
-  raFrom: 3.6,
-  raTo: 4,
-  decFrom: 22,
-  decTo: 25,
-  magLimit: 9,
+  raFrom: 3.3,
+  raTo: 4.5,
+  decFrom: 20,
+  decTo: 27,
+  magLimit: 7,
   width: 600,
   height: 600
 }
 
+const PLEIADES_EYEPIECE_VIEW = {
+  raFrom: 3.73,
+  raTo: 3.85,
+  decFrom: 23.3,
+  decTo: 24.8,
+  magLimit: 11,
+  width: 300,
+  height: 300
+}
 
 
 export const defaultState = {
   stars: [],
-  view: ORION_VIEW,
+  view: PLEIADES_VIEW,
+  eyepieceView: PLEIADES_EYEPIECE_VIEW,
   starStatus: DEFAULT_SERVICE_STATUS
 };
 
